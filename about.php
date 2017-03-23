1f751b95024e817232f47b882faadc84218b56c0
Interference Security <interference-security@users.noreply.github.com>
2017-03-23 13:27:06 +0530

    Updated Empire link

--- about.php
+++ about.php
@@ -13,7 +13,7 @@
             <div class="panel panel-primary">
                 <div class="panel-heading"><span class='glyphicon glyphicon-info-sign'></span> About PowerShell Empire Web v1.0</div>
                 <div class="panel-body">
-                    PowerShell Empire Web utilizes the <a href="https://github.com/PowerShellEmpire/Empire/wiki/RESTful-API">Empire REST API Server</a>. It is a web interface for using <a href="https://github.com/PowerShellEmpire/Empire/">PowerShell Empire</a>.<br><br>
+                    PowerShell Empire Web utilizes the <a href="https://github.com/EmpireProject/Empire/wiki/RESTful-API">Empire REST API Server</a>. It is a web interface for using <a href="https://github.com/EmpireProject/Empire/">PowerShell Empire</a>.<br><br>
                     Empire Web created by <a href="https://twitter.com/xploresec">@xploresec</a><br><br>
                     <a href="https://twitter.com/xploresec"><img src="img/twitter.png" style="width:48px; height:48px;"></a> &nbsp;&nbsp;&nbsp; 
                     <a href="https://github.com/interference-security/empire-web/"><img src="img/github.png" style="width:48px; height:48px;"></a>
